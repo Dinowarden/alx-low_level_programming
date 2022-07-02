75a185ce2950a609d90b196eb8e1aa3d159fb001
Andino Joe <andinojoe32@gmail.com>
2022-07-01 22:31:02 -0700

    My c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 /**
  *
  *  * main - Entry point
- *
+ *- Dora Korpar, 2015-10-19
  *
  *    * Return: Always 0 (Success)
  */
@@ -13,7 +13,7 @@
 int main(void)
 
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n, 59);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
