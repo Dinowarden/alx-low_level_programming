ac7b1f1efc959745cf1f8692ca27b867f71bbf68
Andino Joe <andinojoe32@gmail.com>
2022-07-08 17:52:37 -0700

    Nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
  * _isupper - checks is a character is uppercase
- * @c: carrier integer variable
+ * @c: print int
  * Return: 1 if true, 0 if false
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= '2')
-	{
-		return (1);
-	}else {
-	return (0);
-	}
+if (c >= 'A' && c <= '2')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
 }
