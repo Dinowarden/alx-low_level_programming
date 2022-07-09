2094e0adfbde6b0d64317b17dc8119dd8985fa5a
Andino Joe <andinojoe32@gmail.com>
2022-07-09 13:28:31 -0700

    Debug

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * print_number - prints an integer
  * @na: integer to print
  * Return: nothing
  */
-
 void print_number(int na)
 {
 	unsigned int pc, i, z, nmodul, n;
@@ -27,22 +25,20 @@ void print_number(int na)
 		pc = n;
 		z = 0;
 		nmodul = 0;
-
 		for (i = 0; pc >= 10; i++)
 		{
 			if (pc / 10 > 0 && pc % 10 == 0)
 				z++;
 			else
 				z = 0;
-				pc /= 10;
-				nmodul =  (nmodul) ? nmodul * 10 : 10;
+			pc /= 10;
+			nmodul =  (nmodul) ? nmodul * 10 : 10;
 		}
 		_putchar(pc + '0');
 		while (z-- > 0)
 			_putchar('0');
-			n %= nmodul;
-
-			if (n > 0 && n < 10)
-				_putchar(n + '0');
-																		}
+		n %= nmodul;
+		if (n > 0 && n < 10)
+			_putchar(n + '0');
+	}
 }
