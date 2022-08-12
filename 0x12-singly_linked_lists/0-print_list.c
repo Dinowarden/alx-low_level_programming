2eb7bb7ab682f5637522c0c40364d033bf11e93f
Andino Joe <andinojoe32@gmail.com>
2022-08-11 21:07:59 -0700

    Single ls

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
+
 /**
  * print_list - prints a list
  * @h: head of list
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+
 /**
  * free_list - frees a list
  * @head: head var
